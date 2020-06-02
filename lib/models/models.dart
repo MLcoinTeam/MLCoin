397b7fba96da62e06b5132ee5a3cde84078c15d3
Allan Nava <allannava95@gmail.com>
2020-06-02 10:17:01 +0200

    added models

---
+++ lib/models/models.dart
@@ -0,0 +1,16 @@
+/*
+*  library  - models.dart
+*  mlcoin_app 2020-06-01
+*  mlcoin_app 2020-06-02
+*
+*  Created by [Allan Nava].
+*  Updated by [Allan Nava]
+*  Copyright © 2020 [Allan Nava]. All rights reserved.
+*/
+///
+library models;
+///
+import 'dart:convert';
+import 'package:flutter/material.dart';
+///
+//part 'file.dart';
