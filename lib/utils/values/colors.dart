eab13c3be2a3ace8b1d23fb1bad1934792a9fa85
Filippo Fresilli <illiserf@gmail.com>
2020-06-03 22:06:37 +0200

    adding colors

--- lib/utils/values/colors.dart
+++ lib/utils/values/colors.dart
@@ -10,13 +10,13 @@
 */
 
 import 'dart:ui';
+
 //
 //
 class AppColors {
   static const Color primaryBackground = Color.fromARGB(255, 23, 25, 26);
-  static const Color lineColor         = Color.fromRGBO(35, 35, 35, 1);
+  static const Color lineColor = Color.fromRGBO(35, 35, 35, 1);
   //Primary
-  static const Color primary1     = Color.fromRGBO(30, 167, 255, 1);
-  static const Color primary2     = Color.fromRGBO(255, 255, 255, 1);
-
+  static const Color primary1 = Color(0xffFFFFFF);
+  static const Color primary2 = Color(0xff979DAC);
 }
