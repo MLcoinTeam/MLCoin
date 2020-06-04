7df272871f9621644590601d63927f3988f4df9e
Allan Nava <allannava95@gmail.com>
2020-06-04 22:17:17 +0200

    added splash page need to integrate

---
+++ lib/pages/landing/landing.dart
@@ -0,0 +1,21 @@
+/*
+*  landing.dart
+*  mlcoin_app 2020-04-04
+*  mlcoin_app 2020-06-04
+*
+*  Created by [Allan Nava].
+*  Copyright © 2020 [Allan Nava]. All rights reserved.
+*/
+///
+import 'package:flutter/material.dart';
+///
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //child: child,
+    );
+  }
+}
---
+++ lib/pages/landing/splash.dart
@@ -0,0 +1,22 @@
+/*
+*  splash.dart
+*  mlcoin_app 2020-04-04
+*  mlcoin_app 2020-06-04
+*
+*  Created by [Allan Nava].
+*  Copyright © 2020 [Allan Nava]. All rights reserved.
+*/
+///
+///
+import 'package:flutter/material.dart';
+///
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //child: child,
+    );
+  }
+}
