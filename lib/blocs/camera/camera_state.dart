58d6179a1e96f6cc46e267168f8721134471fb83
Allan Nava <allannava95@gmail.com>
2020-06-15 17:45:15 +0200

    refactor the widget_test but need to integrate the test flow

--- lib/blocs/camera/camera_state.dart
+++ lib/blocs/camera/camera_state.dart
@@ -1,3 +1,14 @@
+/*
+*  camer_bloc.dart
+*  
+*  mlcoin_app 2020-06-04
+*  mlcoin_app 2020-06-15
+*
+*  Created by [Filippo Fresilli & Allan Nava].
+*  Updated by [Filippo Fresilli & Allan Nava]
+*  Copyright © [MLCoinTeam] 2020 . All rights reserved.
+*/
+//
 part of 'camera_bloc.dart';
 
 abstract class CameraState extends Equatable {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,30 @@
+/*
+*  card.dart
+*  
+*  mlcoin_app 2020-06-04
+*  mlcoin_app 2020-06-15
+*
+*  Created by [Filippo Fresilli & Allan Nava].
+*  Updated by [Filippo Fresilli & Allan Nava]
+*  Copyright © [MLCoinTeam] 2020 . All rights reserved.
+*/
+//
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
+//
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mlcoin_app/main.dart';
-
+/// https://flutter.dev/docs/cookbook/testing/unit/introduction
+/// 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  // need to create a test case
+  /*testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
@@ -26,5 +39,5 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
-  });
+  });*/
 }
